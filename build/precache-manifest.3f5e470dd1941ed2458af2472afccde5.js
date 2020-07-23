dc22e6d7251e93b524ad8918017d2101d9513ce9
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-23 21:53:36 +0530

    Add files via upload

---
+++ build/precache-manifest.3f5e470dd1941ed2458af2472afccde5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a552af67ed9796677c03c4da28eb02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12cd7bc15df1a8489a26",
+    "url": "/static/css/main.07005814.chunk.css"
+  },
+  {
+    "revision": "a307f41386da4a210c79",
+    "url": "/static/js/2.30e29773.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.30e29773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12cd7bc15df1a8489a26",
+    "url": "/static/js/main.b14d9019.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  },
+  {
+    "revision": "9d319309f172da6e78c73292f45e2e19",
+    "url": "/static/media/htmlcss.9d319309.jpg"
+  },
+  {
+    "revision": "c4ab2ebcd055d7f1fcacafc78ef1b8b6",
+    "url": "/static/media/jquery.c4ab2ebc.png"
+  },
+  {
+    "revision": "82a9df122485cf07662b058f704ee3e6",
+    "url": "/static/media/js.82a9df12.jpg"
+  },
+  {
+    "revision": "f011876bd1da51cb187e95d1689aad4b",
+    "url": "/static/media/reactjs.f011876b.png"
+  },
+  {
+    "revision": "af95879903c531508fd4544198798174",
+    "url": "/static/media/vuejs.af958799.png"
+  }
+]);
